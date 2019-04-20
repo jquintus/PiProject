67f501d0c7eb900f9ff02b03504e4faec05afd05
Josh Quintus <josh.quintus@gmail.com>
2019-04-20 16:55:46 -0400

    Create webservice to manage volume

--- piboard/server/server.py
+++ piboard/server/server.py
@@ -1,21 +1,48 @@
 from flask import Flask
+from sound import Sound
+
+"""
+Flask webservice that conrols the volume on a windows computer.
+
+Uses https://github.com/Paradoxis/Windows-Sound-Manager for the sound management
+
+To run:
+    python server.py
+"""
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "hello world"
+last_volume = Sound.current_volume()
+
 
 @app.route("/up")
 def up():
-    return "up"
+    Sound.volume_up()
+    current = str(Sound.current_volume())
+    return current
+
 
 @app.route("/down")
 def down():
-    return "down"
+    Sound.volume_down()
+    current = str(Sound.current_volume())
+    return current
+
 
 @app.route("/mute")
 def mute():
-    return "mute"
+    global last_volume
+    current_volume  = Sound.current_volume()
+
+    if current_volume > 0:
+        last_volume = current_volume
+        Sound.volume_min()
+    else:
+        Sound.volume_set(last_volume)
+
+    current = str(Sound.current_volume())
+    return current
+
 
 if __name__ == "__main__":
     app.run()
