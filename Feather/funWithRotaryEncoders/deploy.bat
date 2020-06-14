e849d94a9c69a8eb4b356e12016776bc4dcd2e04
Josh Quintus <josh.quintus@gmail.com>
2020-06-14 15:11:45 -0400

    Add deploy scripts for rotary encoder as well as libs

---
+++ Feather/funWithRotaryEncoders/deploy.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+REM ************************************
+REM Deploy the code file to the 
+REM CircuitPython device
+REM ************************************
+
+copy /Y .\code.py d:\code.py
